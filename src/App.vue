d1c528765b752cac3e8ac28c3ce1c6d03fd2bb92
Jeff Holst <33558430+jeffholst@users.noreply.github.com>
2023-01-30 19:22:23 -0600

    chore: adding interface

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,15 @@
 <script setup lang="ts">
 import HelloWorld from "./components/HelloWorld.vue";
 import SVGBelt from "./components/IconBelt.vue";
+import type BeltProps from "./interfaces/BeltProps";
+
+const beltProps: BeltProps = {
+  level: "1",
+  message: "Hello World",
+  id: "1",
+};
+
+console.log(beltProps);
 </script>
 
 <template>
---
+++ src/interfaces/BeltProps.ts
@@ -0,0 +1,5 @@
+export default interface BeltProps {
+  level: string;
+  message: string;
+  id?: string;
+}
