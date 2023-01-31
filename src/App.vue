87d894393c1a4bfaac4517eef6c93a22412c2e60
Jeff Holst <33558430+jeffholst@users.noreply.github.com>
2023-01-30 21:45:16 -0600

    chore: added deep watcher

--- src/App.vue
+++ src/App.vue
@@ -2,19 +2,27 @@
 import HelloWorld from "./components/HelloWorld.vue";
 import SVGBelt from "./components/IconBelt.vue";
 import type BeltProps from "./interfaces/BeltProps";
+import { reactive, watch } from "vue";
 
-const beltProps: BeltProps = {
+const beltProps: BeltProps = reactive({
   level: "1",
   message: "Hello World",
   id: "1",
-};
+});
 
-console.log(beltProps);
+watch(beltProps, (newValue, oldValue) => {
+  console.log(newValue, oldValue);
+});
+
+const myClick = () => {
+  beltProps.message = "Hello World 2";
+};
 </script>
 
 <template>
   <HelloWorld msg="You did it!" />
   <SVGBelt />
+  <button @click="myClick">Click Me</button>
 </template>
 
 <style scoped></style>
