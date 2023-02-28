34e2efc9f008274e62bc2d693d6f89fbc5775d14
Jeff Holst <33558430+jeffholst@users.noreply.github.com>
2023-02-27 19:24:17 -0600

    fix: customElement warning

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,18 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue({
+      template: {
+        compilerOptions: {
+          // i am ignorning my custom '<container>' tag
+          isCustomElement: (tag) => {
+            return tag.startsWith("rdf");
+          },
+        },
+      },
+    }),
+  ],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
