ba98b583135c646cc7d4e31ae6c1a2399d145ad4
Jeff Holst <33558430+jeffholst@users.noreply.github.com>
2023-03-18 23:00:10 -0500

    chore: added tests for mapColor

--- src/components/__tests__/shared.spec.ts
+++ src/components/__tests__/shared.spec.ts
@@ -1,7 +1,8 @@
 import { describe, it, expect } from "vitest";
 import * as shared from "../../shared/shared";
+import { BeltColor } from "../../types/BeltProps";
 
-describe("Shared tests", () => {
+describe("isValidHexCode", () => {
   const hexCodes: string[] = [
     "#1AFFa1",
     "#F00",
@@ -15,5 +16,38 @@ describe("Shared tests", () => {
     it(`isValidHexCode ${hexCode} (${results[index]})`, () => {
       expect(shared.isValidHexCode(hexCode)).toBe(results[index]);
     });
+  });
+});
+
+describe("mapColor", () => {
+  const colors: BeltColor[] = [
+    {
+      name: "White",
+      hex: "#FFFFFF",
+    },
+    {
+      name: "Gray",
+      hex: "#999999",
+    },
+  ];
+
+  it("should return valid hexcode", () => {
+    expect(shared.mapColor("#FFFFFF", colors)).toBe("#FFFFFF");
+  });
+
+  it("should return mapped value for White", () => {
+    expect(shared.mapColor("White", colors)).toBe("#FFFFFF");
+  });
+
+  it("should return mapped value for mixed case wHiTe", () => {
+    expect(shared.mapColor("wHiTe", colors)).toBe("#FFFFFF");
+  });
+
+  it("should return mapped value for Gray", () => {
+    expect(shared.mapColor("Gray", colors)).toBe("#999999");
+  });
+
+  it("should return passed color", () => {
+    expect(shared.mapColor("Red", colors)).toBe("Red");
   });
 });
