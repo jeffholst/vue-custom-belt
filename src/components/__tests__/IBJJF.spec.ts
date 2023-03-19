4dc0ebd938be67f873e1ee7e7061f57b922fa604
Jeff Holst <33558430+jeffholst@users.noreply.github.com>
2023-03-18 22:27:02 -0500

    chore: added snapshot test for ibjjf.json

--- src/components/__tests__/IBJJF.spec.ts
+++ src/components/__tests__/IBJJF.spec.ts
@@ -6,6 +6,10 @@ import ibjjfJSON from "../../shared/ibjjf.json";
 const ibjjfSystem: BeltSystem = new BeltSystem(ibjjfJSON);
 
 describe("IBJJF System", () => {
+  it("JSON should match", () => {
+    expect(ibjjfSystem).toMatchSnapshot();
+  });
+
   ibjjfJSON.belts.forEach((belt) => {
     it(`${belt.name}`, () => {
       expect(ibjjfSystem.getBeltPropsByName(belt.name, 0)).toMatchSnapshot();
