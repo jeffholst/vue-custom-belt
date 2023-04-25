ce2ae66cd6e6dc3bf78937f838a14dbbc5c2571e
Jeff Holst <33558430+jeffholst@users.noreply.github.com>
2023-04-24 20:17:39 -0500

    chore: comments and missing functions

--- src/types/BeltProps.ts
+++ src/types/BeltProps.ts
@@ -1,8 +1,14 @@
 import * as shared from "../shared/shared";
 
+/**
+ * Where to start stripe placement on patch (Left or Right)
+ */
 export const stripePositions = ["Left", "Right"] as const;
 export type StripePositions = (typeof stripePositions)[number];
 
+/**
+ * Available belt types
+ */
 export const beltTypes = [
   "Solid",
   "Striped",
@@ -13,6 +19,9 @@ export const beltTypes = [
 ] as const;
 export type BeltTypes = (typeof beltTypes)[number];
 
+/**
+ * Property object passed to <CustomBelt /> component
+ */
 export interface BeltProps {
   border: string;
   hasPatch: boolean;
@@ -92,11 +101,17 @@ export interface BeltProps {
   refreshInterval: number;
 }
 
+/**
+ * Used to assign/lookup friendly names to hex colors
+ */
 export interface BeltColor {
   name: string;
   hex: string;
 }
 
+/**
+ * Belt object definition
+ */
 export interface Belt {
   id: number;
   name: string;
@@ -119,6 +134,15 @@ export interface Belt {
   maxStripes: number;
 }
 
+/**
+ * Class used to represent predefined belt systems
+ * @param {string} name name to give belt system
+ * @param {string} title title used for display purposes
+ * @param {string} transitionCSS default transition css to use between belt changes
+ * @param {number} refreshInterval default milliseconds to wait before changing belt
+ * @param {BeltColor[]} colors array of BeltColor objects used to lookup hex colors by friendly name
+ * @param {Belt[]} belts array of Belt objects contained in this belt system
+ */
 export class BeltSystem {
   name: string;
   title: string;
@@ -135,27 +159,42 @@ export class BeltSystem {
     this.colors = system.colors;
     this.belts = system.belts;
 
+    // Replace friendly color names in Belt objects with hex values
     shared.mapColors(this.belts, this.colors);
   }
 
+  /**
+   * Get a Belt by Id
+   * @param {number} id belt id to find
+   * @return {Belt | undefined} matching Belt or undefined
+   */
   getBeltById(id: number): Belt | undefined {
-    // get belt by Id
     return this.belts.find((belt) => belt.id === id);
   }
 
+  /**
+   * Get a Belt by Name (case insensitive search)
+   * @param {string} name belt name to find
+   * @return {Belt | undefined} matching Belt or undefined
+   */
   getBeltByName(name: string): Belt | undefined {
-    // returns Belt matching name ( case insensitive)
     return this.belts.find(
       (belt) => belt.name.toUpperCase() === name.toUpperCase()
     );
   }
 
+  /**
+   * Get BeltpProps for provided Belt object and stripe info
+   * @param {Belt} belt belt object to use
+   * @param {number} stripeCount number of stripes to use (0-10)
+   * @param {StripePositions | undefined} stripePosition stripe placement
+   * @return {BeltProps} BeltProps for provided Belt object and stripe info
+   */
   getBeltProps(
     belt: Belt,
     stripeCount: number,
-    stripePosition: StripePositions | undefined
+    stripePosition: StripePositions | undefined = undefined
   ): BeltProps {
-    // returns BeltProps from provided Belt object and info
     const rdfTitle = `${this.title} ${belt.name} Belt`;
     const rdfDescription = shared.getDescription(rdfTitle, stripeCount);
 
@@ -172,13 +211,21 @@ export class BeltSystem {
     return beltProps;
   }
 
-  getBeltPropsAll(transitionCSS: string, refreshInterval: number): BeltProps[] {
-    // returns BeltProps[] for all belts in the belt system
+  /**
+   * Get all BeltProps from the belt system
+   * @param {string} transitionCSS transition css to use between belt changes. Empty for no effect
+   * @param {number} refreshInterval milliseconds to wait before changing belt. 0 for no rotate
+   * @return {BeltProps[]} all belts in the belt system
+   */
+  getBeltPropsAll(
+    transitionCSS: string = "",
+    refreshInterval: number = 0
+  ): BeltProps[] {
     const beltPropsAry: BeltProps[] = [];
     this.belts.forEach((belt) => {
       const beltProps = this.getBeltProps(
         belt,
-        belt.stripeCount,
+        belt.minStripes,
         belt.stripePosition
       );
       beltProps.transitionCSS = transitionCSS;
@@ -189,12 +236,81 @@ export class BeltSystem {
     return beltPropsAry;
   }
 
+  /**
+   * Get BeltProps for matching belt id
+   * @param {number} id belt id to find
+   * @param {number | undefined} stripeCount number of stripes to use (0-10)
+   * @param {StripePositions | undefined} stripePosition stripe placement (Right or Left)
+   * @return {BeltProps[]} BeltProps for matching belt name
+   */
+  getBeltPropsById(
+    id: number,
+    stripeCount: number | undefined = undefined,
+    stripePosition: StripePositions | undefined = undefined
+  ): BeltProps[] {
+    const beltPropsAry: BeltProps[] = [];
+    const belt = this.getBeltById(id);
+
+    if (belt) {
+      beltPropsAry.push(
+        this.getBeltProps(
+          belt,
+          stripeCount === undefined ? belt.minStripes : stripeCount,
+          stripePosition
+        )
+      );
+    }
+
+    return beltPropsAry;
+  }
+
+  /**
+   * Get BeltProps for matching belt ids
+   * @param {number[]} ids ids of belts to return
+   * @param {number | undefined} stripeCount the number of stripes to use
+   * @param {StripePositions | undefined} stripePosition the stripe position to use
+   * @param {string} transitionCSS transition css to use between belt changes
+   * @param {number} refreshInterval milliseconds to wait before changing belt
+   * @return {BeltProps[]} BeltProps[] of matching belts
+   */
+  getBeltPropsByIds(
+    ids: number[],
+    stripeCount: number | undefined = undefined,
+    stripePosition: StripePositions | undefined = undefined,
+    transitionCSS: string = "",
+    refreshInterval: number = 0
+  ): BeltProps[] {
+    const beltPropsAry: BeltProps[] = [];
+    const belts = this.getBeltsByIds(ids);
+
+    if (belts) {
+      belts.forEach((belt) => {
+        const beltProps: BeltProps = this.getBeltProps(
+          belt,
+          stripeCount === undefined ? belt.minStripes : stripeCount,
+          stripePosition
+        );
+        beltProps.transitionCSS = transitionCSS;
+        beltProps.refreshInterval = refreshInterval;
+        beltPropsAry.push(beltProps);
+      });
+    }
+
+    return beltPropsAry;
+  }
+
+  /**
+   * Get BeltProps for matching belt name
+   * @param {string} name belt name to find
+   * @param {number | undefined} stripeCount number of stripes to use (0-10)
+   * @param {StripePositions | undefined} stripePosition stripe placement (Right or Left)
+   * @return {BeltProps[]} BeltProps for matching belt name
+   */
   getBeltPropsByName(
     name: string,
     stripeCount: number | undefined = undefined,
     stripePosition: StripePositions | undefined = undefined
   ): BeltProps[] {
-    // returns BeltProps[] for specified belt name
     const beltPropsAry: BeltProps[] = [];
     const belt = this.getBeltByName(name);
 
@@ -211,14 +327,22 @@ export class BeltSystem {
     return beltPropsAry;
   }
 
+  /**
+   * Get BeltProps for matching belt names
+   * @param {string[]} names names of belts to return
+   * @param {number | undefined} stripeCount the number of stripes to use
+   * @param {StripePositions | undefined} stripePosition the stripe position to use
+   * @param {string} transitionCSS transition css to use between belt changes
+   * @param {number} refreshInterval milliseconds to wait before changing belt
+   * @return {BeltProps[]} BeltProps[] of matching belts
+   */
   getBeltPropsByNames(
     names: string[],
     stripeCount: number | undefined = undefined,
     stripePosition: StripePositions | undefined = undefined,
-    transitionCSS: string,
-    refreshInterval: number
+    transitionCSS: string = "",
+    refreshInterval: number = 0
   ): BeltProps[] {
-    // returns BeltProps[] for specified belt names
     const beltPropsAry: BeltProps[] = [];
     const belts = this.getBeltsByNames(names);
 
@@ -238,8 +362,29 @@ export class BeltSystem {
     return beltPropsAry;
   }
 
+  /**
+   * Get matching belts by ids
+   * @param {number[]} ids ids of belts to return
+   * @return {Belt[]} Belt[] of matching belts
+   */
+  getBeltsByIds(ids: number[]): Belt[] {
+    const belts: Belt[] = [];
+    ids.forEach((id) => {
+      const b: Belt | undefined = this.getBeltById(id);
+      if (b !== undefined) {
+        belts.push(b);
+      }
+    });
+
+    return belts;
+  }
+
+  /**
+   * Get matching belts by names
+   * @param {string[]} names names of belts to return
+   * @return {Belt[]} Belt[] of matching belts
+   */
   getBeltsByNames(names: string[]): Belt[] {
-    // returns Belt[] for specified belt names (case insensitive)
     const upperCased: string[] = names.map((val) => val.toUpperCase());
     const belts: Belt[] = [];
     upperCased.forEach((belt) => {
